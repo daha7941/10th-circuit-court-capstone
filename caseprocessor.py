9ee771d0e8d29ac01389bb52ebe93ea46b206ba9
Daniel Hartley <danielhartley@Daniels-MacBook-Pro.local>
2017-03-07 16:00:40 -0700

    changed so run_processor returns the set of case_refs

--- caseprocessor.py
+++ caseprocessor.py
@@ -3,8 +3,8 @@ from unidecode import unidecode
 from string import punctuation
 from string import printable
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.cross_validation import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction import text
 
 def init_mongodb():
     #Mongo database and table
@@ -13,20 +13,24 @@ def init_mongodb():
     coll = db.cases
     return db.cases
 
-def nlp_case(incase, intest):
+def nlp_case(incase, urefs):
     '''
     note: need to add specialized stop words still
     '''
-    tfidf = TfidfVectorizer(stop_words='english', max_df=.8, min_df=.01)
+    more_words = set('court', 'defendants', 'defendant', 'plaintiffs', 'plaintiff')
+    my_stop_words = more_words.union(urefs)
+    stop_words = text.ENGLISH_STOP_WORDS.union(my_stop_words)
+    tfidf = TfidfVectorizer(stop_words=stop_words, max_df=.8, min_df=.01)
     vect= tfidf.fit_transform(incase)
-    vtest= tfidf.transform(intest)
+    # vtest= tfidf.transform(intest)
     clf = MultinomialNB(alpha=0.1)
     clf.fit(vect,y)
-    df_pred=clf.predict(vtest)
+    # df_pred=clf.predict(vtest)
     return None
 
 def clean_text(text):
     clean = ''
+    text = unidecode(text)
     for char in text:
         if char in printable and char not in punctuation:
             clean += char.lower()
@@ -40,10 +44,8 @@ def clean_text(text):
 #     # temp = map(unidecode, new_list)
 #     tdf = map(clean_text,temp)
 #     return " ".join(tdf)
-
-if __name__ == '__main__':
+def run_processor():
     cases = init_mongodb()
-    #should this include case ref?
     curs = cases.find() #cursor object
     dlist = list(curs)
     crefs = set()
@@ -51,6 +53,11 @@ if __name__ == '__main__':
         for k,v in i.items():
             for v in i['case_ref']:
                 if len(v)< 30:
-                    x = v.strip()
+                    x = v.strip().lower()
                     crefs.add(x)
             i['case_text']= clean_text(i['case_text'])
+    urefs = map(unidecode, crefs)
+    return dlist, urefs
+
+if __name__ == '__main__':
+    out_dict, out_set = run_processor()
