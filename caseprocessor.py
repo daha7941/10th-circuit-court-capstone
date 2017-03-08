3e8c4bece4a61015fe837d288ab37531cc150d3a
Daniel Hartley <danielhartley@Daniels-MacBook-Pro.local>
2017-03-08 15:40:05 -0700

    lemmatized text, to improve tfidf

--- caseprocessor.py
+++ caseprocessor.py
@@ -1,3 +1,4 @@
+import sys
 from pymongo import MongoClient
 from unidecode import unidecode
 from string import punctuation
@@ -5,6 +6,11 @@ from string import printable
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import text
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+import spacy
+
+if not 'nlp' in locals():
+    nlp = spacy.load('en')
 
 def init_mongodb():
     #Mongo database and table
@@ -17,10 +23,19 @@ def nlp_case(corpus, urefs):
     '''
     note: need to add specialized stop words still
     '''
-    more_words = ['court', 'defendants', 'defendant', 'plaintiffs', 'plaintiff']
+    more_words = ['court', 'defendants', 'defendant', 'plaintiffs', 'plaintiff', 'state', '10th', 'district', 'case', 'federal', 'mr', 'ms', 'dr', 'motion', 'evidence', 'jury', 'united', 'trial', 'ss', \
+        'law', 'rule', 'claim', 'appeal', 'government']
     more_sets = set(more_words)
-    my_stop_words = more_sets.union(urefs)
-    stop_words = text.ENGLISH_STOP_WORDS.union(my_stop_words)
+    my_stop_words = more_sets.union(crefs)
+    new_stop_words = text.ENGLISH_STOP_WORDS.union(my_stop_words)
+
+
+
+    case_text = unicode(case_text.translate(None, punctuation))
+    new_text = nlp(case_text)
+    case_tokens = [token.lemma_.lower() for token in new_text]
+    return ' '.join(x for x in tokens if x not in new_stop_words)
+
     tfidf = TfidfVectorizer(stop_words=stop_words)
     vectors = tfidf.fit_transform(corpus).toarray()
 
@@ -31,13 +46,24 @@ def nlp_case(corpus, urefs):
     # df_pred=clf.predict(vtest)
     return None
 
-def clean_text(text):
-    clean = ''
-    text = unidecode(text)
-    for char in text:
+def clean_text(case_text, crefs):
+    more_words = ['court', 'defendants', 'defendant', 'plaintiffs', 'plaintiff', 'state', '10th', 'district', 'case', 'federal', 'mr', 'ms', 'dr', 'motion', 'evidence', 'jury', 'united', 'trial', 'ss', \
+        'law', 'rule', 'claim', 'appeal', 'government']
+    more_sets = set(more_words)
+    my_stop_words = more_sets.union(crefs)
+    new_stop_words = text.ENGLISH_STOP_WORDS.union(my_stop_words)
+
+    clean_text = ''
+    new_text = unidecode(case_text)
+    # case_text = nlp(case_text)
+    # case_tokens = [token.lemma_.lower() for token in case_text]
+    # return ' '.join(x for x in tokens if x not in new_stop_words)
+    for char in new_text:
         if char in printable and char not in punctuation:
-            clean += char.lower()
-    return clean
+            clean_text += char.lower()
+    token_text = nlp(clean_text)
+    case_tokens = [token.lemma_ for token in token_text]
+    return ' '.join(x for x in case_tokens if x not in new_stop_words)
 
 def run_processor():
     cases = init_mongodb()
@@ -49,15 +75,15 @@ def run_processor():
             for v in i['case_ref']:
                 if len(v)< 30:
                     x = v.strip().lower()
-                    crefs.add(x)
-            i['case_text']= clean_text(i['case_text'])
+                    crefs.add(unidecode(x))
+        i['case_text']= clean_text(i['case_text'], crefs)
     urefs = map(unidecode, crefs)
     #generate corpus
     # corpus = []
     # for item in dlist:
     #     n = item['case_text']
     #     corpus.append(n)
-    return dlist, urefs
+    return dlist, crefs
 
 if __name__ == '__main__':
     out_dict, out_set = run_processor()
