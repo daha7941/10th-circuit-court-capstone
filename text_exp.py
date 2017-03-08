1687cc1d0f1b86f5c5563b8ab8c695398a63e773
Daniel Hartley <danielhartley@Daniels-MacBook-Pro.local>
2017-03-07 17:23:16 -0700

    dropped corpus comparison and defined min/max df for tfidf

--- text_exp.py
+++ text_exp.py
@@ -8,20 +8,20 @@ def main_exp(corpus, urefs):
     '''
     note: need to add (more?) specialized stop words still
     '''
-    more_words = ['court', 'defendants', 'defendant', 'plaintiffs', 'plaintiff']
+    more_words = ['court', 'defendants', 'defendant', 'plaintiffs', 'plaintiff', 'state', '10th', 'district', 'case', 'federal', 'mr', 'ms', 'dr', 'motion', 'evidence', 'jury', 'united', 'trial', 'ss', \
+    'law', 'rule', 'claim', 'appeal', 'government']
     more_sets = set(more_words)
     my_stop_words = more_sets.union(urefs)
     stop_words = text.ENGLISH_STOP_WORDS.union(my_stop_words)
-    tfidf = TfidfVectorizer(stop_words=stop_words)
-    tfidf2 = TfidfVectorizer(stop_words=stop_words, use_idf=False)
+    tfidf = TfidfVectorizer(stop_words=stop_words, max_df=0.9, min_df=0.05)
     vectors = tfidf.fit_transform(corpus).toarray()
 
     #top 20 words
-    # top_words(tfidf, tfidf2, vectors, corpus, 20)
+    top_words(tfidf, vectors, corpus, 20)
 
     cos_corp(vectors, corpus)
-
-def top_words(tfidf, tfidf2, vectors, data, n):
+    # return vectors
+def top_words(tfidf, vectors, data, n):
 
     words = tfidf.get_feature_names()
 
@@ -35,11 +35,6 @@ def top_words(tfidf, tfidf2, vectors, data, n):
     print get_values(total, n, words)
     print
 
-    vectors2 = tfidf2.fit_transform([" ".join(data)]).toarray()
-    print "top %d by tf across all corpus" % n
-    print get_values(vectors2[0], n, words)
-    print
-
 def get_values(lst, n, labels):
     #sorts list and returns n values
     return [labels[i] for i in np.argsort(lst)[-1:-n-1:-1]]
@@ -50,9 +45,9 @@ def cos_corp(vectors, corpus):
     cosine_similarities = linear_kernel(vectors, vectors)
     for i, case1 in enumerate(corpus):
         for j, case2 in enumerate(corpus):
-            #limit prints to those above value and different cases
+            # limit prints to those above value and different cases
             if i != j:
-                if cosine_similarities[i,j] > 0.09:
+                if cosine_similarities[i,j] > 0.9:
                     print i,j, cosine_similarities[i,j]
 
 if __name__ == '__main__':
