fdd6cf44ba6f9945c6f52bd45e872dc653c9b211
Daniel Hartley <danielhartley@Daniels-MacBook-Pro.local>
2017-03-13 17:05:40 -0600

    now loads pickled files instead of running case process each time

--- text_exp.py
+++ text_exp.py
@@ -1,31 +1,40 @@
-from caseprocessor import run_processor
+import cPickle as pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import text
 from sklearn.metrics.pairwise import linear_kernel
 import numpy as np
 
-def main_exp(corpus, urefs):
+def main_exp():
     '''
     note: need to add (more?) specialized stop words still
     '''
+    with open('case_pickle.txt','rb') as f:
+        new_dict = pickle.load(f)
+    with open('ref_pickle.txt','rb') as fp:
+        new_set = pickle.load(fp)
+    corpus = []
+    for item in new_dict:
+        n = item['case_text']
+        corpus.append(n)
     # more_words = ['court', 'defendants', 'defendant', 'plaintiffs', 'plaintiff', 'state', '10th', 'district', 'case', 'federal', 'mr', 'ms', 'dr', 'motion', 'evidence', 'jury', 'united', 'trial', 'ss', \
     # 'law', 'rule', 'claim', 'appeal', 'government']
     # more_sets = set(more_words)
     # my_stop_words = more_sets.union(urefs)
     # stop_words = text.ENGLISH_STOP_WORDS.union(my_stop_words)
-    tfidf = TfidfVectorizer(stop_words=urefs, max_df=0.9, min_df=0.05)
-    vectors = tfidf.fit_transform(corpus).toarray()
-    return vectors
+    tfidf = TfidfVectorizer(stop_words=new_set, max_df=0.9, min_df=0.05, max_features=5000)
+    vectors = tfidf.fit_transform(corpus)
+    return tfidf, vectors, corpus
+
 def top_words(tfidf, vectors, data, n):
 
     words = tfidf.get_feature_names()
-
-    avg = np.sum(vectors, axis=0) / np.sum(vectors > 0, axis=0)
+    new_vect = vectors.toarray()
+    avg = np.sum(vectors, axis=0) / np.sum(new_vect > 0, axis=0)
     print "top %d by average tf-idf" % n
     print get_values(avg, n, words)
     print
 
-    total = np.sum(vectors, axis=0)
+    total = np.sum(new_vect, axis=0)
     print "top %d by total tf-idf" % n
     print get_values(total, n, words)
     print
@@ -37,26 +46,19 @@ def get_values(lst, n, labels):
 
 def cos_corp(vectors, corpus):
     #find cosine similarity between all cases in the corpus
-    cosine_similarities = linear_kernel(vectors, vectors)
+    new_vect = vectors.toarray()
+    cosine_similarities = linear_kernel(new_vect, new_vect)
     for i, case1 in enumerate(corpus):
         for j, case2 in enumerate(corpus):
             # limit prints to those above value and different cases
             if i != j:
-                if cosine_similarities[i,j] > 0.9:
-                    print i,j, cosine_similarities[i,j]
+                print i,j, cosine_similarities[i,j]
 
 if __name__ == '__main__':
-    out_dict, out_set = run_processor()
 
-    #generate corpus
-    corpus = []
-    for item in out_dict:
-        n = item['case_text']
-        corpus.append(n)
-
-    vect = main_exp(corpus,out_set)
+    tfidf, vectors, corpus = main_exp()
 
     #top 20 words
-    top_words(tfidf, vectors, corpus, 20)
+    # top_words(tfidf, vectors, corpus, 20)
 
     cos_corp(vectors, corpus)
