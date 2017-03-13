287a4f8a71879bcc4b456c5186cedc2d4a1a63d7
Daniel Hartley <danielhartley@Daniels-MacBook-Pro.local>
2017-03-13 17:04:24 -0600

    case process changes

--- nmf_matrix.py
+++ nmf_matrix.py
@@ -1,19 +1,23 @@
-from caseprocessor import run_processor
+from text_exp import main_exp
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import cPickle as pickle
 
-def nmf_topic_matrix(corpus, urefs, id_list):
-    tfidf = TfidfVectorizer(stop_words=urefs, max_df=0.9, min_df=0.05,max_features=5000)
-    tfidf_vectors = tfidf.fit_transform(corpus)
+def nmf_topic_matrix(corpus, vectors, tfidf):
+    tfidf_vectors = vectors
     dm = tfidf_vectors.toarray()
     vcb = np.array(tfidf.get_feature_names())
     nmf = NMF(n_components= 10,random_state=1, alpha=0.1, l1_ratio=0.5)
     case_topic = nmf.fit_transform(dm)
 
+    top_words(nmf, vcb, 20)
+    return case_topic
+
+def case_rec(case_topic, id_list):
     case_topic = case_topic / np.sum(case_topic, axis=1, keepdims=True)
     id_list = np.asarray(id_list)
     case_topic_ori = case_topic.copy()
@@ -23,19 +27,23 @@ def nmf_topic_matrix(corpus, urefs, id_list):
         case_topic_grouped[i,:]=np.mean(case_topic[id_list==case,:],axis=0)
     return case_topic_grouped
 
+def top_words(model, feature_names, n_words):
+    for i, v in enumerate(model.components_):
+        print ('Topic {0}').format(i)
+        print(' '.join(feature_names[x] for x in v.argsort()[:-n_words - 1:-1]))
+        print()
+
 if __name__ == '__main__':
-    out_dict, out_set = run_processor()
-
-    #generate corpus
-    corpus = []
+    tfidf, vectors, corpus = main_exp()
+    with open('case_pickle.txt','rb') as f:
+        new_dict = pickle.load(f)
     id_list = []
-    for item in out_dict:
-        n = item['case_text']
-        case_id = item['_id']
-        corpus.append(n)
-        id_list.append(case_id)
+    for item in new_dict:
+        n = item['_id']
+        id_list.append(n)
 
-    matz = nmf_topic_matrix(corpus, out_set, id_list)
-    test_df = pd.DataFrame(matz, index = id_list)
+
+    case_topic = nmf_topic_matrix(corpus, vectors, tfidf)
+    case_matrix = case_rec(case_topic, id_list)
+    test_df = pd.DataFrame(case_matrix, index = id_list)
     test_df['class']=test_df.idxmax(axis=1)
-    
